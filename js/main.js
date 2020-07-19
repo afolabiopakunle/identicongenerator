e92470b84253917c2643428b49d53544755c86df
Afolabi Opakunle <opakunleafolabi@gmail.com>
2020-07-19 18:10:23 +0100

    moved app to js template

--- js/main.js
+++ js/main.js
@@ -12,5 +12,16 @@ new Vue({
     identicon() {
       return jdenticon.toSvg(this.inputData, 340)
     }
-  }
+  },
+  template: `
+  <div>
+  <div>
+  <h1 style="font-family: Sacramento;">Identicon Icon Generator</h1>
+  <input placeholder="Enter some texts..." type="text" v-on:input="onInput" autofocus>
+</div>
+<div class="display">
+  <p v-html="identicon"></p>
+</div>
+</div>
+  `
 })
